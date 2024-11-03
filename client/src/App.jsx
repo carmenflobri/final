ac9f4d61e43d793edab5da9c02d90319b8412fa5
carmenflobri <carmenflobri@alumnos.cei.es>
2024-11-03 23:08:39 +0100

    Primer commit - subiendo proyecto de React y Express

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Signup';
+import Home from './Home';
+import New from './New';
+import Myprofile from './Myprofile';
+
+function App() {
+  return (
+    <BrowserRouter> 
+      <Routes>
+        <Route path='/register' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/new' element={<New />} />
+        <Route path='/myprofile' element={<Myprofile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
